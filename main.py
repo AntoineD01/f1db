8a652cc6fce0a2ff009fd302f5a16aa0800a113b
Antoine Dupont <tony_dupont@yahoo.com>
2024-05-05 17:15:03 +0200

    displaying all the circuits

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import file_management as fm
 
 
 def main():
-    file_path = r"circuits\adelaide.yml"
-    yaml_data = fm.open_yaml_file(file_path)
-    d.display_circuit(yaml_data)
     nb_file = fm.count_files_in_folder(r"C:\Users\Antoine Dupont\Desktop\F1 Stats Project\f1db\circuits")
-    #for i in range(nb_file):
+    titles = fm.get_file_titles_in_folder(r"C:\Users\Antoine Dupont\Desktop\F1 Stats Project\f1db\circuits")
+    for title in titles:
+        file_path = "circuits\\" + title + ".yml"
+        yaml_data = fm.open_yaml_file(file_path)
+        d.display_circuit(yaml_data)
+
 
 # Example usage:
 if __name__ == "__main__":
