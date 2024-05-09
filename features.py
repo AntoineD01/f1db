afdd9bcbefeb419c2d792c99703b0aee920fab32
Antoine Dupont <tony_dupont@yahoo.com>
2024-05-09 14:38:00 +0200

    search improved

--- features.py
+++ features.py
@@ -1,3 +1,4 @@
+from fuzzywuzzy import fuzz
 
 def get_valid_input(question, accepted_values):
     while True:
@@ -18,32 +19,34 @@ def two_part_input(data):
 
 def search(database, data):
     found = False
-    index = None  
+    index = None 
+    
+    # Look for an exact match
     for i, sdata in enumerate(database):
         if sdata.get('name', '') == data:
             found = True
             index = i
             break
-    #Look with a capital letter at first
-    data = data.capitalize()
-    for i, sdata in enumerate(database):
-        if sdata.get('name', '') == data:
-            found = True
-            index = i
-            break
-    #Look with full caps
-    data = data.upper() 
-    for i, sdata in enumerate(database):
-        if sdata.get('name', '') == data:
-            found = True
-            index = i
-            break
-    #Look with caps on each part of the input
-    valid, capdata = two_part_input(data)
-    if valid:
-        for i, sdata in enumerate(database):
-            if sdata.get('name', '') == capdata:
+
+    # Look for closest match
+    if found == False:
+        closest_match, index = search_closest_match(data, [sdata.get('name', '') for sdata in database])
+        if closest_match is not None:
+            result = get_valid_input(f"Did you meant {closest_match} ? (1 for yes or 2 for no)", ["1","2"])
+            if result == 1:
                 found = True
-                index = i
-                break
     return found, index
+
+def search_closest_match(user_input, database):
+    best_match = None
+    best_score = 0
+    best_index = -1
+    index = -1
+    for item in database:
+        index += 1
+        score = fuzz.ratio(user_input, item)
+        if score > best_score:
+            best_match = item
+            best_score = score
+            best_index = index
+    return best_match, best_index
